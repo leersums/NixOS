1374ba8fc24c9bfc42af6d6be9099e05a7df7d72
leersums <s.van.leersum@xs4all.nl>
2024-02-04 01:33:46 +0100

    home-manager was added to the flake.nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }@inputs:
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
     {
       nixosConfigurations = {
         keoldale = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [ ./configuration.nix];
+          modules = [ ./configuration.nix
+                      home-manager.nixosModules.home-manager
+                      {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.useUserPackages = true;
+                        home-manager.users.leersums = import ./home/leersums.nix;
+                        home-manager.users.leersumta = import ./home/leersumta.nix;
+                        # Optionally, use home-manager.extraSpecialArgs to pass
+                        # arguments to home.nix
+                      }
+                    ];
         };
         thurso = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
